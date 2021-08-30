b969f5231150cb29997348544cb58f8821f36177
Jason <jstarkey0919@gmail.com>
2021-08-29 22:33:03 -0400

    sfsfs

---
+++ TypeScript/examples.ts
@@ -0,0 +1,23 @@
+
+
+// Defining an Interface
+interface User {
+    name: string;
+    age: number;
+}
+
+//  Applying an Interface
+let user : User = {
+    name: "Jason",
+    age: 22,
+}
+
+
+
+class UserClass {
+    name: string;
+    age: number;
+
+
+    
+}
